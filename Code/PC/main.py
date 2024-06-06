92c038972cde9d1a1db7d52d4683cbf0541928ab
Antov Iliyan <AntovI@e-btlnet.local>
2024-06-06 17:45:46 +0300

    Added more UI functionality and made a sample data .json

--- Code/PC/main.py
+++ Code/PC/main.py
@@ -1,8 +1,12 @@
-from PyQt5.QtCore import QRegularExpression
+import os
+import time
+
+from PyQt5.QtCore import QRegularExpression, Qt, QSize, QEventLoop, QTimer, pyqtSignal, QObject
+from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import *
 
 from MainWindowUI import Ui_Form
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtTest
 import sys
 
 
@@ -23,14 +27,23 @@ class MainWindowLogic:
             "max": 1000
         }
 
+        self.channel_enabled = [False, False, False, False]
+        self.data_processing_enabled = [False, False, False, False]
+        self.temperature_prediction_enabled = [False, False, False, False]
+        self.prediction_processing_enabled = [False, False, False, False]
+
+        self.record_folder_dir = "./data/"
+
     def run(self):
         self.ui.setupUi(self.form)
         self.widgets = self.form.children()
 
         self.update_enabled_widgets()
         self.assign_button_functions()
+        self.display_available_records()
 
         self.form.show()
+
         sys.exit(self.app.exec_())
 
     def update_enabled_widgets(self):
@@ -49,7 +62,6 @@ class MainWindowLogic:
             if widget is not None:
                 widget.setEnabled(status)
 
-        channel_parameters_widgets = []
         for widget_index in range(self.ui.ChannelParametersLayout.count()):
             widget = self.ui.ChannelParametersLayout.itemAt(widget_index).widget()
             if widget is None:
@@ -58,82 +70,106 @@ class MainWindowLogic:
                 widget.setEnabled(True)
             else:
                 widget.setEnabled(False)
-                channel_parameters_widgets.append(widget)
 
+        # Enable channels
         if status:
-            self.update_channel_widgets_enable_status()
+            channel_enable_checkbox_names = QRegularExpression("ChannelEnabled_\d")
+            channel_enable_checkbox_widgets = self.form.findChildren(QCheckBox, channel_enable_checkbox_names)
+            for widget in channel_enable_checkbox_widgets:
+                widget.setEnabled(True)
 
-    def update_channel_widgets_enable_status(self):
-        data_processing_checkbox_names = QRegularExpression(r"Data.*Enabled_\d")
-        data_processing_checkbox_widgets = sorted(self.form.findChildren(QCheckBox, data_processing_checkbox_names), key=lambda x: x.objectName())
-        prediction_checkbox_names = QRegularExpression(r"PredictionEnabled_\d")
-        prediction_checkbox_widgets = sorted(self.form.findChildren(QCheckBox, prediction_checkbox_names), key=lambda x: x.objectName())
-        prediction_processing_checkbox_names = QRegularExpression(r"Prediction.+Enabled_\d")
-        prediction_processing_checkbox_widgets = sorted(self.form.findChildren(QCheckBox, prediction_processing_checkbox_names), key=lambda x: x.objectName())
+            for i in range(4):
+                self.update_channel_widgets_enable_status(i + 1)
 
-        tc_type_names = QRegularExpression(r"ChannelTcType_\d")
-        tc_type_widgets = sorted(self.form.findChildren(QComboBox, tc_type_names), key=lambda x: x.objectName())
+    def update_channel_widgets_enable_status(self, channel):
+        channel_index = channel - 1
 
-        channel_enabled_checkbox_names = QRegularExpression(r"ChannelEnabled_\d")
-        channel_enabled_checkbox_widgets = sorted(self.form.findChildren(QCheckBox, channel_enabled_checkbox_names), key=lambda x: x.objectName())
-        for widget in channel_enabled_checkbox_widgets:
-            widget.setEnabled(True)
+        # Channel enabled
+        tc_type_selector_name = f"ChannelTcType_{channel}"
+        tc_type_selector_widget = self.form.findChild(QComboBox, tc_type_selector_name)
+        checkbox_object_names = QRegularExpression(fr"[^ChannelEnabled].*_{channel}")
+        checkbox_object_widgets = self.form.findChildren(QCheckBox, checkbox_object_names)
+        # Enable/Disable relevant objects
+        tc_type_selector_widget.setEnabled(self.channel_enabled[channel_index])
+        for widget in checkbox_object_widgets:
+            if "PostProcess" in widget.objectName():
+                if not self.temperature_prediction_enabled[channel_index]:
+                    self.prediction_processing_enabled[channel_index] = False
+                    widget.setChecked(False)
+                    widget.setEnabled(False)
+                    continue
+            widget.setEnabled(self.channel_enabled[channel_index])
 
-        for i in range(len(data_processing_checkbox_widgets)):
-            data_processing_checkbox_widgets[i].setEnabled(channel_enabled_checkbox_widgets[i].isChecked())
-            prediction_checkbox_widgets[i].setEnabled(channel_enabled_checkbox_widgets[i].isChecked())
-            prediction_processing_checkbox_widgets[i].setEnabled(channel_enabled_checkbox_widgets[i].isChecked())
-            tc_type_widgets[i].setEnabled(channel_enabled_checkbox_widgets[i].isChecked())
+        # Data pre-processing
+        data_processing_names = QRegularExpression(fr"DataLowPass.*_{channel}")
+        data_processing_widgets = self.form.findChildren((QSpinBox, QDoubleSpinBox), data_processing_names)
+        # Enable/Disable relevant objects
+        for widget in data_processing_widgets:
+            widget.setEnabled(self.data_processing_enabled[channel_index] and self.channel_enabled[channel_index])
 
-        data_processing_names = QRegularExpression(r"Data.*_\d")
-        data_processing_widgets = sorted(self.form.findChildren((QSpinBox, QDoubleSpinBox), data_processing_names), key=lambda x: x.objectName())
-        for i in range(len(data_processing_widgets)):
-            if channel_enabled_checkbox_widgets[i % 4].isChecked():
-                data_processing_widgets[i].setEnabled(data_processing_checkbox_widgets[i % 4].isChecked())
-            else:
-                data_processing_widgets[i].setEnabled(False)
+        # Temperature prediction
+        temperature_prediction_names = QRegularExpression(fr"Prediction.*_{channel}")
+        temperature_prediction_widgets = self.form.findChildren((QSpinBox, QDoubleSpinBox), temperature_prediction_names)
+        # Enable/Disable relevant objects
+        for widget in temperature_prediction_widgets:
+            widget.setEnabled(self.temperature_prediction_enabled[channel_index] and self.channel_enabled[channel_index])
 
-        prediction_names = QRegularExpression(r"Prediction[^LowPass].*_\d")
-        prediction_widgets = sorted(self.form.findChildren((QSpinBox, QDoubleSpinBox), prediction_names), key=lambda x: x.objectName())
-        for i in range(len(prediction_widgets)):
-            if channel_enabled_checkbox_widgets[i % 4].isChecked():
-                prediction_widgets[i].setEnabled(prediction_checkbox_widgets[i % 4].isChecked())
-            else:
-                prediction_widgets[i].setEnabled(False)
+        # Prediction post-processing
+        post_processing_names = QRegularExpression(fr"PostProcess.*_{channel}")
+        post_processing_widgets = self.form.findChildren((QSpinBox, QDoubleSpinBox), post_processing_names)
+        # Enable/Disable relevant objects
+        for widget in post_processing_widgets:
+            widget.setEnabled(self.prediction_processing_enabled[channel_index] and self.channel_enabled[channel_index])
 
-        prediction_processing_names = QRegularExpression(r"PredictionLowPass.*_\d")
-        prediction_processing_widgets = sorted(self.form.findChildren((QSpinBox, QDoubleSpinBox), prediction_processing_names), key=lambda x: x.objectName())
-        for i in range(len(prediction_widgets)):
-            if channel_enabled_checkbox_widgets[i % 4].isChecked():
-                prediction_processing_widgets[i].setEnabled(prediction_processing_checkbox_widgets[i % 4].isChecked())
-            else:
-                prediction_processing_widgets[i].setEnabled(False)
+    def toggle_channel(self, channel):
+        channel_index = channel - 1
+        self.channel_enabled[channel_index] = not self.channel_enabled[channel_index]
+        self.update_channel_widgets_enable_status(channel)
 
+    def toggle_data_processing(self, channel):
+        channel_index = channel - 1
+        self.data_processing_enabled[channel_index] = not self.data_processing_enabled[channel_index]
+        self.update_channel_widgets_enable_status(channel)
+
+    def toggle_temperature_prediction(self, channel):
+        channel_index = channel - 1
+        self.temperature_prediction_enabled[channel_index] = not self.temperature_prediction_enabled[channel_index]
+        self.update_channel_widgets_enable_status(channel)
+
+    def toggle_prediction_processing(self, channel):
+        channel_index = channel - 1
+        self.prediction_processing_enabled[channel_index] = not self.prediction_processing_enabled[channel_index]
+        self.update_channel_widgets_enable_status(channel)
 
     def assign_button_functions(self):
         self.ui.ConnectionButton.clicked.connect(self.vcp_connection_change)
-        self.ui.ChannelEnabled_1.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.ChannelEnabled_2.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.ChannelEnabled_3.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.ChannelEnabled_4.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.DataLowPassEnabled_1.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.DataLowPassEnabled_2.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.DataLowPassEnabled_3.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.DataLowPassEnabled_4.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionEnabled_1.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionEnabled_2.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionEnabled_3.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionEnabled_4.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionLowPassEnabled_1.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionLowPassEnabled_2.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionLowPassEnabled_3.clicked.connect(self.update_channel_widgets_enable_status)
-        self.ui.PredictionLowPassEnabled_4.clicked.connect(self.update_channel_widgets_enable_status)
+        self.ui.MeasureButton.clicked.connect(self.measure)
+
+        self.ui.ChannelEnabled_1.clicked.connect(lambda: self.toggle_channel(1))
+        self.ui.ChannelEnabled_2.clicked.connect(lambda: self.toggle_channel(2))
+        self.ui.ChannelEnabled_3.clicked.connect(lambda: self.toggle_channel(3))
+        self.ui.ChannelEnabled_4.clicked.connect(lambda: self.toggle_channel(4))
+        self.ui.DataLowPassEnabled_1.clicked.connect(lambda: self.toggle_data_processing(1))
+        self.ui.DataLowPassEnabled_2.clicked.connect(lambda: self.toggle_data_processing(2))
+        self.ui.DataLowPassEnabled_3.clicked.connect(lambda: self.toggle_data_processing(3))
+        self.ui.DataLowPassEnabled_4.clicked.connect(lambda: self.toggle_data_processing(4))
+        self.ui.PredictionEnabled_1.clicked.connect(lambda: self.toggle_temperature_prediction(1))
+        self.ui.PredictionEnabled_2.clicked.connect(lambda: self.toggle_temperature_prediction(2))
+        self.ui.PredictionEnabled_3.clicked.connect(lambda: self.toggle_temperature_prediction(3))
+        self.ui.PredictionEnabled_4.clicked.connect(lambda: self.toggle_temperature_prediction(4))
+        self.ui.PostProcessLowPassEnabled_1.clicked.connect(lambda: self.toggle_prediction_processing(1))
+        self.ui.PostProcessLowPassEnabled_2.clicked.connect(lambda: self.toggle_prediction_processing(2))
+        self.ui.PostProcessLowPassEnabled_3.clicked.connect(lambda: self.toggle_prediction_processing(3))
+        self.ui.PostProcessLowPassEnabled_4.clicked.connect(lambda: self.toggle_prediction_processing(4))
         record_length_widget = self.ui.RecordLengthValue
         self.ui.RecordLengthMinButton.clicked.connect(lambda: record_length_widget.setValue(self.record_length_min_max["min"]))
         self.ui.RecordLengthMaxButton.clicked.connect(lambda: record_length_widget.setValue(self.record_length_min_max["max"]))
         record_interval_widget = self.ui.RecordIntervalValue
         self.ui.RecordIntervalMinButton.clicked.connect(lambda: record_interval_widget.setValue(self.record_interval_min_max["min"]))
         self.ui.RecordIntervalMaxButton.clicked.connect(lambda: record_interval_widget.setValue(self.record_interval_min_max["max"]))
+
+        self.ui.RenameRecordButton.clicked.connect(self.rename_selected_record)
+        self.ui.DeleteRecordButton.clicked.connect(self.delete_selected_record)
 
     def vcp_connection_change(self):
         if self.connection_established:
@@ -145,6 +181,100 @@ class MainWindowLogic:
             self.ui.ConnectionButton.setText("Disconnect")
             # self.vcp_connect()
         self.update_enabled_widgets()
+
+    def measure(self):
+        self.form.setEnabled(False)
+
+        number_of_steps = 3
+        progress_dialog = QProgressDialog("Preparing...", "Cancel", 0, number_of_steps + 1, self.form)
+        progress_dialog.overrideWindowFlags(Qt.Window | Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+        progress_dialog.setWindowModality(Qt.WindowModal)
+        progress_dialog.setWindowTitle("Measuring")
+        progress_dialog.setCancelButton(None)
+        progress_dialog.setAttribute(Qt.WA_DeleteOnClose)
+        progress_dialog.show()
+
+        progress_dialog.setValue(1)
+        progress_dialog.setLabelText("Sending setup to MCU...")
+        self.measurement_send_setup()
+        progress_dialog.setLabelText("Receiving data from MCU...")
+        progress_dialog.setValue(2)
+        self.measurement_receive_data()
+        progress_dialog.setLabelText("Processing received data...")
+        progress_dialog.setValue(3)
+        self.measurement_process_data()
+        progress_dialog.setLabelText("Done")
+        progress_dialog.setValue(4)
+
+        self.form.setEnabled(True)
+
+        return
+
+    def measurement_send_setup(self):
+        # Send UI settings to MCU
+        loop = QEventLoop()
+        QTimer.singleShot(1000, loop.quit)
+        loop.exec_()
+        return
+
+    def measurement_receive_data(self):
+        # Receive data from MCU
+        loop = QEventLoop()
+        QTimer.singleShot(1000, loop.quit)
+        loop.exec_()
+        return
+
+    def measurement_process_data(self):
+        # Process and save received data
+        loop = QEventLoop()
+        QTimer.singleShot(1000, loop.quit)
+        loop.exec_()
+        return
+
+    def display_available_records(self):
+        self.ui.RecordsList.clear()
+        self.ui.RecordsList.setItemAlignment(Qt.AlignRight)
+        record_list = os.listdir(self.record_folder_dir)
+        for record in record_list:
+            item = QListWidgetItem(record.replace(".json", ""))
+            item.setTextAlignment(Qt.AlignRight)
+            self.ui.RecordsList.addItem(item)
+
+    def rename_selected_record(self):
+        selected_record = self.ui.RecordsList.currentItem()
+        if not selected_record:
+            return
+
+        current_name = selected_record.text()
+        new_name, ok = QtWidgets.QInputDialog.getText(self.ui.RecordsList,
+                                                      "Rename record",
+                                                      "New record name:",
+                                                      QLineEdit.Normal,
+                                                      current_name)
+        if not (ok and new_name):
+            return
+
+        os.rename(str(self.record_folder_dir + current_name + ".json"), str(self.record_folder_dir + new_name + ".json"))
+        self.display_available_records()
+
+    def delete_selected_record(self):
+        selected_record = self.ui.RecordsList.currentItem()
+        if not selected_record:
+            return
+
+        selected_record_name = selected_record.text()
+
+        messagebox = QMessageBox(QMessageBox.Question,
+                                 "Confirm delete",
+                                 "Are you sure?",
+                                 QMessageBox.Yes | QMessageBox.No)
+        response = messagebox.exec()
+
+        if response == QMessageBox.No:
+            return
+
+        os.remove(str(self.record_folder_dir + selected_record_name + ".json"))
+        self.display_available_records()
 
 
 if __name__ == "__main__":
