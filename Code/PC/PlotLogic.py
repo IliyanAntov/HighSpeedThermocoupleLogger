65714d2b8d28b6ce6c5d64e4bb3fbe1c939e1773
Antov Iliyan <AntovI@e-btlnet.local>
2024-06-07 18:39:51 +0300

    Added plotting functionality (data filtering, predicted curve generation, predicted curve filtering)

--- Code/PC/PlotLogic.py
+++ Code/PC/PlotLogic.py
@@ -1,10 +1,15 @@
 import copy
 import json
+import math
 import sys
+from functools import partial
 
 import numpy as np
+from scipy import signal
+
 from PyQt5.QtCore import QRegularExpression
 from PyQt5.QtWidgets import *
+from scipy.optimize import curve_fit
 
 from PC.Record import Record
 from PC.SharedParameters import SharedParameters
@@ -23,7 +28,6 @@ class PlotLogic:
         self.record = Record()
         self.record_saved = Record()
         self.available_channel_records = []
-        self.changes_made = False
 
     def run(self, record_file_name):
         self.ui.setupUi(self.form)
@@ -63,11 +67,14 @@ class PlotLogic:
 
             self.update_channel_widgets_enable_status(i + 1)
 
+    def change_apply_cancel_buttons_enable_status(self, state):
+        self.ui.ApplyButton.setEnabled(state)
+        self.ui.CancelButton.setEnabled(state)
+
     def update_channel_widgets_enable_status(self, channel):
         channel_index = channel - 1
 
-        self.ui.ApplyButton.setEnabled(self.changes_made)
-        self.ui.CancelButton.setEnabled(self.changes_made)
+        self.change_apply_cancel_buttons_enable_status(False)
 
         # Channel enabled
         checkbox_object_names = QRegularExpression(fr"[^ChannelEnabled].*_{channel}")
@@ -108,28 +115,64 @@ class PlotLogic:
             widget.setEnabled(self.record.channels[channel_index].prediction_processing_enabled and self.record.channels[channel_index].available)
 
     def toggle_channel(self, channel):
-        self.changes_made = True
         channel_index = channel - 1
         self.record.channels[channel_index].available = not self.record.channels[channel_index].available
         self.update_channel_widgets_enable_status(channel)
+        self.change_apply_cancel_buttons_enable_status(True)
 
     def toggle_data_processing(self, channel):
-        self.changes_made = True
         channel_index = channel - 1
         self.record.channels[channel_index].data_processing_enabled = not self.record.channels[channel_index].data_processing_enabled
         self.update_channel_widgets_enable_status(channel)
+        self.change_apply_cancel_buttons_enable_status(True)
 
     def toggle_temperature_prediction(self, channel):
-        self.changes_made = True
         channel_index = channel - 1
         self.record.channels[channel_index].temperature_prediction_enabled = not self.record.channels[channel_index].temperature_prediction_enabled
         self.update_channel_widgets_enable_status(channel)
+        self.change_apply_cancel_buttons_enable_status(True)
 
     def toggle_prediction_processing(self, channel):
-        self.changes_made = True
         channel_index = channel - 1
         self.record.channels[channel_index].prediction_processing_enabled = not self.record.channels[channel_index].prediction_processing_enabled
         self.update_channel_widgets_enable_status(channel)
+        self.change_apply_cancel_buttons_enable_status(True)
+
+    def data_filter_order_changed(self, channel):
+        channel_index = channel - 1
+        widget = self.form.findChild(QSpinBox, f"DataLowPassOrder_{channel}")
+        self.record.channels[channel_index].data_filter_order = widget.value()
+        self.change_apply_cancel_buttons_enable_status(True)
+
+    def data_filter_frequency_changed(self, channel):
+        channel_index = channel - 1
+        widget = self.form.findChild(QDoubleSpinBox, f"DataLowPassCornerFrequency_{channel}")
+        self.record.channels[channel_index].data_filter_freq_khz = widget.value()
+        self.change_apply_cancel_buttons_enable_status(True)
+
+    def prediction_time_constant_changed(self, channel):
+        channel_index = channel - 1
+        widget = self.form.findChild(QDoubleSpinBox, f"PredictionTimeConstant_{channel}")
+        self.record.channels[channel_index].prediction_time_constant_ms = widget.value()
+        self.change_apply_cancel_buttons_enable_status(True)
+
+    def prediction_queue_length_changed(self, channel):
+        channel_index = channel - 1
+        widget = self.form.findChild(QSpinBox, f"PredictionQueueLength_{channel}")
+        self.record.channels[channel_index].prediction_queue_length = widget.value()
+        self.change_apply_cancel_buttons_enable_status(True)
+
+    def prediction_filter_order_changed(self, channel):
+        channel_index = channel - 1
+        widget = self.form.findChild(QSpinBox, f"PostProcessLowPassOrder_{channel}")
+        self.record.channels[channel_index].post_process_filter_order = widget.value()
+        self.change_apply_cancel_buttons_enable_status(True)
+
+    def prediction_filter_frequency_changed(self, channel):
+        channel_index = channel - 1
+        widget = self.form.findChild(QDoubleSpinBox, f"PostProcessLowPassCornerFrequency_{channel}")
+        self.record.channels[channel_index].post_process_filter_freq_khz = widget.value()
+        self.change_apply_cancel_buttons_enable_status(True)
 
     def assign_button_functions(self):
         self.ui.ChannelEnabled_1.clicked.connect(lambda: self.toggle_channel(1))
@@ -148,6 +191,31 @@ class PlotLogic:
         self.ui.PostProcessLowPassEnabled_2.clicked.connect(lambda: self.toggle_prediction_processing(2))
         self.ui.PostProcessLowPassEnabled_3.clicked.connect(lambda: self.toggle_prediction_processing(3))
         self.ui.PostProcessLowPassEnabled_4.clicked.connect(lambda: self.toggle_prediction_processing(4))
+
+        self.ui.DataLowPassOrder_1.editingFinished.connect(lambda: self.data_filter_order_changed(1))
+        self.ui.DataLowPassOrder_2.editingFinished.connect(lambda: self.data_filter_order_changed(2))
+        self.ui.DataLowPassOrder_3.editingFinished.connect(lambda: self.data_filter_order_changed(3))
+        self.ui.DataLowPassOrder_4.editingFinished.connect(lambda: self.data_filter_order_changed(4))
+        self.ui.DataLowPassCornerFrequency_1.editingFinished.connect(lambda: self.data_filter_frequency_changed(1))
+        self.ui.DataLowPassCornerFrequency_2.editingFinished.connect(lambda: self.data_filter_frequency_changed(2))
+        self.ui.DataLowPassCornerFrequency_3.editingFinished.connect(lambda: self.data_filter_frequency_changed(3))
+        self.ui.DataLowPassCornerFrequency_4.editingFinished.connect(lambda: self.data_filter_frequency_changed(4))
+        self.ui.PredictionTimeConstant_1.editingFinished.connect(lambda: self.prediction_time_constant_changed(1))
+        self.ui.PredictionTimeConstant_2.editingFinished.connect(lambda: self.prediction_time_constant_changed(2))
+        self.ui.PredictionTimeConstant_3.editingFinished.connect(lambda: self.prediction_time_constant_changed(3))
+        self.ui.PredictionTimeConstant_4.editingFinished.connect(lambda: self.prediction_time_constant_changed(4))
+        self.ui.PredictionQueueLength_1.editingFinished.connect(lambda: self.prediction_queue_length_changed(1))
+        self.ui.PredictionQueueLength_2.editingFinished.connect(lambda: self.prediction_queue_length_changed(2))
+        self.ui.PredictionQueueLength_3.editingFinished.connect(lambda: self.prediction_queue_length_changed(3))
+        self.ui.PredictionQueueLength_4.editingFinished.connect(lambda: self.prediction_queue_length_changed(4))
+        self.ui.PostProcessLowPassOrder_1.editingFinished.connect(lambda: self.prediction_filter_order_changed(1))
+        self.ui.PostProcessLowPassOrder_2.editingFinished.connect(lambda: self.prediction_filter_order_changed(2))
+        self.ui.PostProcessLowPassOrder_3.editingFinished.connect(lambda: self.prediction_filter_order_changed(3))
+        self.ui.PostProcessLowPassOrder_4.editingFinished.connect(lambda: self.prediction_filter_order_changed(4))
+        self.ui.PostProcessLowPassCornerFrequency_1.editingFinished.connect(lambda: self.prediction_filter_frequency_changed(1))
+        self.ui.PostProcessLowPassCornerFrequency_2.editingFinished.connect(lambda: self.prediction_filter_frequency_changed(2))
+        self.ui.PostProcessLowPassCornerFrequency_3.editingFinished.connect(lambda: self.prediction_filter_frequency_changed(3))
+        self.ui.PostProcessLowPassCornerFrequency_4.editingFinished.connect(lambda: self.prediction_filter_frequency_changed(4))
 
         self.ui.ViewPlotButton.clicked.connect(self.plot)
         self.ui.ApplyButton.clicked.connect(self.update_parameters)
@@ -228,7 +296,6 @@ class PlotLogic:
         self.record_saved = copy.deepcopy(self.record)
 
     def update_parameters(self):
-        self.changes_made = False
         self.record_saved = copy.deepcopy(self.record)
         for i in range(self.record.num_of_channels):
             if self.record.channels[i].available:
@@ -236,7 +303,6 @@ class PlotLogic:
         self.update_enabled_widgets()
 
     def restore_saved_parameters(self):
-        self.changes_made = False
         self.record = copy.deepcopy(self.record_saved)
         for i in range(self.record.num_of_channels):
             if self.record.channels[i].available:
@@ -244,20 +310,93 @@ class PlotLogic:
         self.update_enabled_widgets()
 
     def plot(self):
+        self.restore_saved_parameters()
+
         channel_colors = ["red", "blue", "green", "orange"]
         x_values = np.linspace(0, self.record.length_ms, int(self.record.length_ms/(self.record.interval_us/1000)))
+        y_max = None
         for i in range(self.record.num_of_channels):
             if not self.record.channels[i].available:
                 continue
-            y_values = self.record.channels[i].raw_data
-            plt.plot(x_values, y_values, color=channel_colors[i], label=f"Channel {i + 1}")
+
+            # Data plot
+            if not self.record.channels[i].data_processing_enabled:
+                y_data_values = self.record.channels[i].raw_data
+                data_label = f"Channel {i + 1} data (No filter)"
+            else:
+                data = self.record.channels[i].raw_data
+                filter_order = self.record.channels[i].data_filter_order
+                filter_corner_freq_khz = self.record.channels[i].data_filter_freq_khz
+                y_data_values = self.apply_filter(data, filter_order, filter_corner_freq_khz)
+                data_label = f"Channel {i + 1} data ({filter_corner_freq_khz} kHz, order {filter_order} filter)"
+
+            if y_max is None or max(y_data_values) > y_max:
+                y_max = max(y_data_values)
+
+            plt.plot(x_values, y_data_values, color=channel_colors[i], label=data_label)
+
+            # Prediction plot
+            if not self.record.channels[i].temperature_prediction_enabled:
+                continue
+
+            prediction_values = PlotLogic.calculate_predicted_data(x_values, y_data_values, self.record.channels[i])
+
+            if not self.record.channels[i].prediction_processing_enabled:
+                y_prediction_values = prediction_values
+                prediction_label = f"Channel {i + 1} prediction (No filter)"
+            else:
+                filter_order = self.record.channels[i].post_process_filter_order
+                filter_corner_freq_khz = self.record.channels[i].post_process_filter_freq_khz
+                y_prediction_values = self.apply_filter(prediction_values, filter_order, filter_corner_freq_khz)
+                prediction_label = f"Channel {i + 1} prediction ({filter_corner_freq_khz} kHz, order {filter_order} filter)"
+
+            if max(y_prediction_values) > y_max:
+                y_max = max(y_prediction_values)
+
+            x_values_prediction = x_values[:len(x_values) - self.record.channels[i].prediction_queue_length]
+            plt.plot(x_values_prediction, y_prediction_values, color=channel_colors[i], linestyle="dashed", label=prediction_label)
 
         plt.title(self.record_file_name.replace(".json", ""), pad=15)
         plt.xlabel("Time [ms]")
+        plt.xticks(np.linspace(0, self.record.length_ms, 11))
         plt.ylabel("Temperature [°C]")
+        y_ticks = math.ceil(y_max / 10.0)
+        plt.yticks(np.linspace(0, y_ticks * 10, y_ticks + 1))
         plt.legend()
+        plt.grid()
 
         plt.show()
+
+    def apply_filter(self, data, order, corner_frequency_khz):
+        cutoff_freq_khz = (1 / self.record.interval_us) * 1000
+        nyquist_freq_khz = cutoff_freq_khz / 2
+        b, a = signal.butter(N=order, Wn=corner_frequency_khz / nyquist_freq_khz)
+        data_filtered = signal.filtfilt(b, a, data)
+        return data_filtered.tolist()
+
+    @staticmethod
+    def heating_function(x, t_env, tau_ms, t_0):
+        return t_env + ((t_0 - t_env) * np.exp(-(1 / (tau_ms / 1000)) * x))
+
+    @staticmethod
+    def calculate_predicted_data(x_data, y_data, channel_parameters):
+        x_short = x_data[:channel_parameters.prediction_queue_length]
+        y_short = y_data[:channel_parameters.prediction_queue_length]
+        predictions = []
+
+        for data_point in y_data[channel_parameters.prediction_queue_length:]:
+            t_0 = y_short[0]
+            heating_function_fixed_tau = partial(PlotLogic.heating_function, tau_ms=channel_parameters.prediction_time_constant_ms, t_0=t_0)
+
+            param, param_cov = curve_fit(heating_function_fixed_tau, x_short, y_short)
+            predictions.append(param[0])
+
+            y_short.pop(0)
+            y_short.append(data_point)
+
+        # predictions += [predictions[-1]] * channel_parameters.prediction_queue_length
+
+        return predictions
 
 # TODO: delete
 if __name__ == "__main__":
